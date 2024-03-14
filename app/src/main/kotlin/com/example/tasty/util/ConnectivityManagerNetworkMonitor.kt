ef6554559418517479a5f03bffdf136eb418d03e
Stevan Milovanovic <stevanm993@gmail.com>
2024-03-14 18:07:33 +0100

    Refactor connectivity manager network monitor

--- app/src/main/kotlin/com/example/tasty/util/ConnectivityManagerNetworkMonitor.kt
+++ app/src/main/kotlin/com/example/tasty/util/ConnectivityManagerNetworkMonitor.kt
@@ -8,66 +8,62 @@ import android.net.NetworkRequest
 import androidx.core.content.getSystemService
 import com.example.tasty.data.network.NetworkMonitor
 import dagger.hilt.android.qualifiers.ApplicationContext
-import javax.inject.Inject
+import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.conflate
+import javax.inject.Inject
 
 class ConnectivityManagerNetworkMonitor @Inject constructor(
-	@ApplicationContext private val context: Context,
+    @ApplicationContext private val context: Context
 ) : NetworkMonitor {
-	override val isOnline: Flow<Boolean> = callbackFlow {
-		val connectivityManager = context.getSystemService<ConnectivityManager>()
-		if (connectivityManager == null) {
-			channel.trySend(false)
-			channel.close()
-			return@callbackFlow
-		}
+    override val isOnline: Flow<Boolean> = callbackFlow {
+        val connectivityManager = context.getSystemService<ConnectivityManager>()
+        if (connectivityManager == null) {
+            channel.trySend(false)
+            channel.close()
+            return@callbackFlow
+        }
+        val callback = getNetworkCallback(channel)
+        val request = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .build()
+        connectivityManager.registerNetworkCallback(request, callback)
+        sendConnectivityStatusToChannel(channel, connectivityManager.isCurrentlyConnected())
+        awaitClose { connectivityManager.unregisterNetworkCallback(callback) }
+    }.conflate()
 
-		/**
-		 * The callback's methods are invoked on changes to *any* network matching the [NetworkRequest],
-		 * not just the active network. So we can simply track the presence (or absence) of such [Network].
-		 */
-		/**
-		 * The callback's methods are invoked on changes to *any* network matching the [NetworkRequest],
-		 * not just the active network. So we can simply track the presence (or absence) of such [Network].
-		 */
-		val callback = object : ConnectivityManager.NetworkCallback() {
+    private fun ConnectivityManager.isCurrentlyConnected() = activeNetwork
+        ?.let(::getNetworkCapabilities)
+        ?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
 
-			private val networks = mutableSetOf<Network>()
+    /**
+     * Sends the latest connectivity status to the underlying channel.
+     */
+    private fun sendConnectivityStatusToChannel(
+        channel: SendChannel<Boolean>,
+        isCurrentlyConnected: Boolean
+    ) {
+        channel.trySend(isCurrentlyConnected)
+    }
 
-			override fun onAvailable(network: Network) {
-				networks += network
-				channel.trySend(true)
-			}
+    /**
+     * The callback's methods are invoked on changes to *any* network matching the [NetworkRequest],
+     * not just the active network. So we can simply track the presence (or absence) of such [Network].
+     */
+    private fun getNetworkCallback(channel: SendChannel<Boolean>) = object : ConnectivityManager.NetworkCallback() {
 
-			override fun onLost(network: Network) {
-				networks -= network
-				channel.trySend(networks.isNotEmpty())
-			}
-		}
+        private val networks = mutableSetOf<Network>()
 
-		val request = NetworkRequest.Builder()
-			.addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-			.build()
-		connectivityManager.registerNetworkCallback(request, callback)
+        override fun onAvailable(network: Network) {
+            networks += network
+            channel.trySend(true)
+        }
 
-		/**
-		 * Sends the latest connectivity status to the underlying channel.
-		 */
-
-		/**
-		 * Sends the latest connectivity status to the underlying channel.
-		 */
-		channel.trySend(connectivityManager.isCurrentlyConnected())
-
-		awaitClose {
-			connectivityManager.unregisterNetworkCallback(callback)
-		}
-	}.conflate()
-
-	private fun ConnectivityManager.isCurrentlyConnected() = activeNetwork
-		?.let(::getNetworkCapabilities)
-		?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
+        override fun onLost(network: Network) {
+            networks -= network
+            channel.trySend(networks.isNotEmpty())
+        }
+    }
 }
