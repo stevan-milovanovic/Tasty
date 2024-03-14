ab18c3c6c2cf01047a35396d1a6d7a0fc5e108f9
Stevan Milovanovic <stevanm993@gmail.com>
2024-03-14 18:07:03 +0100

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
 	implementation("androidx.core:core-ktx:1.12.0")
 	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 	implementation("androidx.activity:activity-compose:1.8.2")
-	implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+	implementation(platform("androidx.compose:compose-bom:2024.02.02"))
 	implementation("androidx.compose.ui:ui")
 	implementation("androidx.navigation:navigation-compose:2.7.7")
 	implementation("androidx.compose.ui:ui-graphics")
@@ -87,7 +87,7 @@ dependencies {
 	implementation("io.coil-kt:coil-svg:2.5.0")
 
 	//exoplayer
-	val mediaVersion = "1.2.1"
+	val mediaVersion = "1.3.0"
 	implementation("androidx.media3:media3-exoplayer:$mediaVersion")
 	implementation("androidx.media3:media3-ui:$mediaVersion")
 	implementation("androidx.media3:media3-common:$mediaVersion")
@@ -119,7 +119,7 @@ dependencies {
 	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-	androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+	androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.02"))
 	androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 	debugImplementation("androidx.compose.ui:ui-tooling")
 	debugImplementation("androidx.compose.ui:ui-test-manifest")
